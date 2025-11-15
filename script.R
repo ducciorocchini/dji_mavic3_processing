796d7e474f353cf566373faba7cdc9d61802cbfe
Duccio Rocchini <ducciorocchini@gmail.com>
2025-11-15 13:22:18 +0100

    Update script.R

--- script.R
+++ script.R
@@ -14,6 +14,29 @@ stack <- c(nir, red, green)
 plot(stack)
 im.plotRGB(stack, 1, 2, 3)
 
+
+# Assign CRS to both images (assuming WGS 84)
+crs(nir) <- "EPSG:4326"
+crs(red) <- "EPSG:4326"
+crs(green) <- "EPSG:4326"
+
+# Check if the CRS is now set correctly
+crs(nir)
+crs(red)
+crs(green)
+
+# Check extent of both images
+ext(nir)
+ext(red)
+ext(green)
+
+# If they don't match, align the extent (for example, use the extent of the red image)
+ext(nir) <- ext(red)
+ext(green) <- ext(red)
+
+stack <- c(nir, red, green)
+im.plotRGB(stack, 1, 2, 3)
+
 #---- 
 
 setwd("/Users/ducciorocchini/Desktop/dji_mavic3/immagine_2/TIF")
@@ -27,3 +50,31 @@ im2 <- c(nir, rededge, red, green)
 
 plot(im2)
 im.plotRGB(im2, 3, 4, 3)
+
+library(imager)
+
+# Check the extents and CRS of both images (to align them)
+ext(red)
+ext(nir)
+
+# Assign CRS to both images (assuming WGS 84)
+crs(nir) <- "EPSG:4326"
+crs(red) <- "EPSG:4326"
+crs(green) <- "EPSG:4326"
+
+# Check if the CRS is now set correctly
+crs(nir)
+crs(red)
+crs(green)
+
+# Check extent of both images
+ext(nir)
+ext(red)
+ext(green)
+
+# If they don't match, align the extent (for example, use the extent of the red image)
+ext(nir) <- ext(red)
+ext(green) <- ext(red)
+
+stack <- c(nir, red, green)
+im.plotRGB(stack, 1, 2, 3)
